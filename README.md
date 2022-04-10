30c029b7fb64d5b9d27d4a26528596352648adcc
Damian Cangaris <damian@cangaris.pl>
2022-04-10 15:59:41 +0200

    @PathVariable, @RequestParam, required = false|true, Model, th:text, th:replace, xmlns:th="http://www.thymeleaf.org"

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://www.baeldung.com/spring-thymeleaf-fragments
+https://www.baeldung.com/spring-requestparam-vs-pathvariable
--- src/main/java/pl/cansoft/spring3/HelloController.java
+++ src/main/java/pl/cansoft/spring3/HelloController.java
@@ -1,10 +1,21 @@
 package pl.cansoft.spring3;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class HelloController {
+
+    Map<String, String> blogMap = Map.of(
+        "czerwiec", "Jakiś artykuł z miesiąca Czerwiec",
+        "lipiec", "Jakiś artykuł z miesiąca Lipiec",
+        "sierpień", "Jakiś artykuł z miesiąca Sierpień"
+    );
 
     @GetMapping
     public String hello() {
@@ -22,7 +33,22 @@ public class HelloController {
     }
 
     @GetMapping("blog")
-    public String blog() {
+    public String blog(@RequestParam(required = false) String m, Model model) {
+        if (m != null) {
+            // sprawdzam null ponieważ required = false (m może być nullem)
+            // jeżeli required = true - sprawdzanie nie ma sensu (m nigdy nie będzie nullem)
+            var value = blogMap.get(m);
+            model.addAttribute("content", value);
+        }
+        model.addAttribute("month", m);
+        return "blog/blog";
+    }
+
+    @GetMapping("blog/{month}")
+    public String blogPath(@PathVariable String month, Model model) {
+        var value = blogMap.get(month);
+        model.addAttribute("content", value);
+        model.addAttribute("month", month);
         return "blog/blog";
     }
 }
